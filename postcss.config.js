8db4ac1b61661fe2b3357f07714ef2ff2e6232a9
Kyle Pollich <kpollich1@gmail.com>
2020-05-22 23:05:24 -0400

    Fix tailwind config

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,7 @@
 const purgecss = [
   "@fullhuman/postcss-purgecss",
   {
-    content: ["./components/**/*.js", "./pages/**/*.js"],
+    content: ["src/**/*.tsx"],
     defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
   },
 ];
