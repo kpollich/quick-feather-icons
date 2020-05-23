a8db8c2f1cc0c1933b273020bb851b51a739f957
Kyle Pollich <kpollich1@gmail.com>
2020-05-23 14:48:15 -0400

    Cleanup unused files. Add meta title + descriptiong

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,26 @@
 import React from "react";
 import App from "next/app";
+import Head from "next/head";
 
 import "../styles/tailwind.css";
 
 class MyApp extends App {
   render() {
     const { Component, pageProps } = this.props;
-    return <Component {...pageProps} />;
+
+    return (
+      <>
+        <Head>
+          <title>Quick Feather Icons</title>
+          <meta
+            name="description"
+            content="Quickly copy links to Feather icons for use in Notion and other apps"
+          />
+        </Head>
+
+        <Component {...pageProps} />
+      </>
+    );
   }
 }
 
