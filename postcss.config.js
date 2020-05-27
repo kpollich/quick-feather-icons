e9a9ff032771bad2a590f6f2e197411fe7da9a82
Kyle Pollich <kpollich1@gmail.com>
2020-05-27 19:00:01 -0400

    Remove PurgeCSS

--- postcss.config.js
+++ postcss.config.js
@@ -1,15 +1,3 @@
-const purgecss = [
-  "@fullhuman/postcss-purgecss",
-  {
-    content: ["src/**/*.tsx"],
-    defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
-  },
-];
 module.exports = {
-  plugins: [
-    "postcss-import",
-    "tailwindcss",
-    "autoprefixer",
-    ...(process.env.NODE_ENV === "production" ? [purgecss] : []),
-  ],
+  plugins: ["postcss-import", "tailwindcss", "autoprefixer"],
 };
